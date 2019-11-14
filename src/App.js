7a8e0031032385e225391dbd2b0c8db385604c72
Michael Weedman <michael.weedman@gmail.com>
2019-11-14 10:40:40 -0700

    Passed down, adjusting state.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
-import React, { Component } from 'react';
-import List from './List'
-import './App.css';
+import React, { Component } from "react";
+import List from "./List";
+import "./App.css";
 
 class App extends Component {
   static defaultProps = {
     store: {
       lists: [],
-      allCards: {},
+      allCards: {}
     }
+  };
+  state = {
+    store: this.props.store
+  };
+
+  deleteFunction = id => {
+    const newCards = Object.values(this.state.store.allCards);
+    console.log(newCards);
+    newCards.filter(newCards.cardId => newCards.cardId !== newCards.id);
+    console.log(newCards);
+    this.setState({
+      Store: { allCards: newCards }
+    });
   };
 
   render() {
-    const { store } = this.props
     return (
-      <main className='App'>
-        <header className='App-header'>
+      <main className="App">
+        <header className="App-header">
           <h1>Trelloyes!</h1>
         </header>
-        <div className='App-list'>
-          {store.lists.map(list => (
+        <div className="App-list">
+          {this.state.store.lists.map(list => (
             <List
               key={list.id}
               header={list.header}
-              cards={list.cardIds.map(id => store.allCards[id])}
+              cards={list.cardIds.map(id => this.state.store.allCards[id])}
+              delete={this.deleteFunction}
             />
           ))}
         </div>
